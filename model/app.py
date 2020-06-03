b2c4a04ce4e8c54b6fed6b01dff1a73c51cca17d
Raphael Tang <raphpb1912@gmail.com>
2020-06-03 18:51:14 +0800

    remove cleanup from model

--- model/app.py
+++ model/app.py
@@ -22,7 +22,7 @@ app.add_middleware(CORSMiddleware, allow_origins=["*"])
 @app.post("/")
 async def root(body: RootBody):
     result: str = ai.generate_one(
-        prompt=body.prefix, max_length=body.max_length, temperature=0.7, top_p=0.9, repetition_penalty=1.2
+        prompt=body.prefix, max_length=body.max_length, temperature=0.7, top_p=0.9, repetition_penalty=1.2, cleanup=False
     )
 
     return {"text": result}
